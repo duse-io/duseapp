f4ca600fd1dd55d7849bdd0c0fd5bff6d4005e48
Adracus <adracus@gmail.com>
2015-02-11 10:19:04 +0100

    Temp commit

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,6 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:html';
-
 import 'package:duseapp/component/login.dart';
 import 'package:duseapp/annotations.dart';
 
@@ -11,19 +9,21 @@ import 'package:angular/application_factory.dart';
 import 'package:duse/duse.dart';
 import 'package:restpoint/restpoint_client_client.dart';
 
-var client = new DuseClient(Uri.parse("https://duse.herokuapp.com/v1"), new ClientClientFactory());
+final client = new DuseClient(Uri.parse("https://duse.herokuapp.com/v1"),
+    new ClientClientFactory());
 
 
 class DuseAppModule extends Module {
   DuseAppModule() {
-    bind(LoginComponent);
     bind(DuseClient, withAnnotation: duseClient, toValue: client);
+    bind(LoginComponent);
   }
 }
 
 
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
-  applicationFactory().addModule(new DuseAppModule()).run();
+  applicationFactory()
+    .addModule(new DuseAppModule())
+    .run();
 }
